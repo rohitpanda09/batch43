a3bab30fce1b4585e114f8e0019c84ed02b63f7a
rohitpanda09 <67759802+rohitpanda09@users.noreply.github.com>
2026-04-25 20:17:41 +0530

    Fix formatting of printed message in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print the message to the console
-        System.out.println("Hello, Prajapati and Yogesh, Jay,Rohit from batch-43!");
+        System.out.println("Hello, Prajapati and Yogesh, Jay, Rohit from batch-43!");
     }
 }
