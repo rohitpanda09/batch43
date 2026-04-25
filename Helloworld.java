b4eb1ab49ed541960806164e963b87e4e5416d28
rohitpanda09 <67759802+rohitpanda09@users.noreply.github.com>
2026-04-25 19:41:59 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
+// A simple Java program to print a custom message
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Prince and yogesh , jay from batch-43!");
+        // Print the message to the console
+        System.out.println("Hello, Prince and Yogesh, Jay from batch-43!");
     }
 }
